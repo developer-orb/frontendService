a7591f64e75fe851ab723691b5aa8f4771f8811f
octa.quintero <octa.quinteroo@gmail.com>
2024-05-31 20:40:26 -0300

    Define initial page structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from '../src/store/store';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
+} else {
+  console.error('No se encontró el elemento con id "root" en el DOM.');
+}
